a9aee97091b5f3b24ca6ae23975f567080b89696
ArtShare Prototype <153028313+ArtSharePrototype@users.noreply.github.com>
2023-12-05 15:59:12 -0600

    Add files via upload

---
+++ background-animation.js
@@ -0,0 +1,65 @@
+const canvas = document.getElementById('animation-canvas');
+const ctx = canvas.getContext('2d');
+
+let gradientShift = 0;
+
+function refreshAnimation() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight+100;
+
+  // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //generate random particles
+  const particles = [];
+  const particleCount = 300; // increase the number of particles
+  const particleSize = 16;
+
+  for (let i = 0; i < particleCount; i++) {
+    const particle = {
+      x: i * (canvas.width / particleCount),
+      y: canvas.height / 1.5,
+      size: Math.random() * particleSize + 1,
+      speedX: Math.random() * 3 - 1.5,
+      speedY: Math.random() * 3 - 1.5,
+      shape: 'circle',
+    };
+
+    particles.push(particle);
+  }
+
+  //animate
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //draw partacle
+    particles.forEach((particle, index) => {
+      //set color
+      const gradient = ctx.createLinearGradient(gradientShift, 0, gradientShift + canvas.width, 0);
+      gradient.addColorStop(0, '#EDD700');
+      gradient.addColorStop(0.5, '#0DDBCF');
+      gradient.addColorStop(1, '#EDD700');
+      ctx.fillStyle = gradient;
+
+      // draw circle for each particle
+      ctx.beginPath();
+      ctx.arc(particle.x, particle.y + Math.sin((Date.now() + index * 100) / 1000) * 275, particle.size / 2, 0, Math.PI * 2);
+      ctx.closePath();
+      ctx.fill();
+    });
+
+    // shift gradient
+    gradientShift += 1;
+    if (gradientShift > canvas.width) {
+      gradientShift = -canvas.width;
+    }
+
+    //call next frame
+    requestAnimationFrame(animate);
+  }
+
+  //begin to animate
+  animate();
+}
+
+refreshAnimation();
+window.addEventListener('resize', refreshAnimation);
